ebc071fbbb72d24a2d1aa5d85c9321181c428880
TanaNobo <hwkrk923@yahoo.co.jp>
2022-07-16 22:08:29 +0900

    get rover data func at backend

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -13,6 +13,33 @@ app.use(bodyParser.json())
 app.use('/', express.static(path.join(__dirname, '../public')))
 
 // your API calls
+/* app.get('/mars', async (req, res) =>{
+    try {
+        const manifest = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        res.send({ manifest })
+    } catch (err) {
+        console.log('error:', err);
+    }
+}) */
+
+const rovers_names = ['Curiosity', 'Opportunity', 'Spirit']
+for (const name of rovers_names){
+    app.get(`/mars/${name}`, async (req, res) =>{
+        try {
+            const manifest = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${name}?api_key=${process.env.API_KEY}`)
+                .then(res => res.json())
+            const max_sol = manifest.photo_manifest.max_sol;
+
+            const contents = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${name}/photos?sol=${max_sol}&api_key=${process.env.API_KEY}`)
+                .then(res => res.json())
+            res.send({ contents })
+        } catch (err) {
+            console.log('error:', err);
+        }
+    })
+}
+
 
 // example API call
 app.get('/apod', async (req, res) => {
